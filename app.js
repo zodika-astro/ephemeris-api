c694630503b1ebcf43b1055e53067df9d98f484e
Isadora Rocha <info@zodika.com.br>
2025-06-11 23:08:54 +0300

    fix: ensure Basic-Auth middleware loads before routes

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// --- Basic Auth ---
+var basicAuth = require('express-basic-auth');
+var USER = process.env.BASIC_USER;
+var PASS = process.env.BASIC_PASS;
+
+app.use(basicAuth({
+  users: { [USER]: PASS },
+  challenge: true
+}));
+// -------------------
 var express = require('express');
 var logger = require('morgan');
 var responseHandler = require('./common/responseHandlers');
