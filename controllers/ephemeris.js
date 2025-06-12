fdee8a5a908dd94240db34a04f93ef0be127d94a
Isadora Rocha <info@zodika.com.br>
2025-06-12 15:12:06 +0300

    feat: add real Sun calculation with swisseph

--- controllers/ephemeris.js
+++ controllers/ephemeris.js
@@ -1,24 +1,38 @@
 'use strict';
 
+const swisseph = require('swisseph');
+
 module.exports = {
   compute: function (query) {
-    const result = {
-      ephemerisQuery: query,
-      ephemerides: {
-        geo: {
-          1: [
-            {
-              longitude: 273.45,
-              latitude: -4.91,
-              planet: 1,
-              model: 'geo'
-            }
-          ]
-        }
-      }
-    };
+    return new Promise((resolve, reject) => {
+      const { year, month, date, hours, minutes, seconds, latitude, longitude } = query;
 
-    return Promise.resolve(result);
+      const jd = swisseph.swe_julday(year, month, date, hours + minutes / 60 + seconds / 3600, swisseph.SE_GREG_CAL);
+
+      swisseph.swe_calc_ut(jd, swisseph.SE_SUN, 0, (err, planet) => {
+        if (err) {
+          return reject(err);
+        }
+
+        const result = {
+          ephemerisQuery: query,
+          ephemerides: {
+            geo: {
+              1: [
+                {
+                  longitude: planet.longitude,
+                  latitude: planet.latitude,
+                  planet: 1,
+                  model: 'geo'
+                }
+              ]
+            }
+          }
+        };
+
+        resolve(result);
+      });
+    });
   }
 };
 
