a760365a6c9954afa33e55da776087721b847654
Isadora Rocha <info@zodika.com.br>
2025-06-12 15:56:30 +0300

    debug: log input values for julday

--- controllers/ephemeris.js
+++ controllers/ephemeris.js
@@ -18,25 +18,48 @@ module.exports = {
         config
       } = reqBody;
 
+      console.log('📦 Inputs recebidos:');
+      console.log('year:', year);
+      console.log('month:', month);
+      console.log('date:', date);
+      console.log('hours:', hours);
+      console.log('minutes:', minutes);
+      console.log('seconds:', seconds);
+      console.log('latitude:', latitude);
+      console.log('longitude:', longitude);
+      console.log('timezone:', timezone);
+
+      const decimalHours = hours + minutes / 60 + seconds / 3600;
+
       const jd = swisseph.swe_julday(
         year,
         month,
         date,
-        hours + minutes / 60 + seconds / 3600,
+        decimalHours,
         swisseph.SE_GREG_CAL
       );
+
+      console.log('🧮 Julian Day calculado:', jd);
+
+      // Exemplo: cálculo para o Sol (planeta 0)
+      const eph = await new Promise((resolve, reject) => {
+        swisseph.swe_calc(jd, swisseph.SE_SUN, 0, (res) => {
+          if (res.error) reject(res.error);
+          else resolve(res);
+        });
+      });
 
       return {
         ephemerisQuery: reqBody,
         ephemerides: {
           geo: {
-            1: [
+            0: [ // Sol
               {
-                longitude: 273.45,
-                latitude: -4.91,
-                planet: 1,
-                model: 'geo',
-                jd: jd
+                longitude: eph.longitude,
+                latitude: eph.latitude,
+                distance: eph.distance,
+                planet: 0,
+                model: 'geo'
               }
             ]
           }
