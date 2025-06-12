783e02df9bbeb629c7cf46eaa8da4b7cf604872e
Isadora Rocha <info@zodika.com.br>
2025-06-12 13:20:40 +0300

    feat: add mock implementation of ephemeris compute function

--- controllers/ephemeris.js
+++ controllers/ephemeris.js
@@ -1,9 +1,27 @@
 'use strict';
 
-exports.getEphemeris = function (req, res, next) {
-  res.locals.status = 200;
-  res.locals.message = 'Ephemeris endpoint is working';
-  res.locals.results = { info: 'This is just a placeholder response.' };
-  next();
+module.exports = {
+  compute: function (req, res, next) {
+    const result = {
+      ephemerisQuery: req.body || {},
+      ephemerides: {
+        geo: {
+          1: [
+            {
+              longitude: 273.45,
+              latitude: -4.91,
+              planet: 1,
+              model: 'geo'
+            }
+          ]
+        }
+      }
+    };
+
+    res.locals.status = 200;
+    res.locals.message = 'Ephemerides returned';
+    res.locals.results = result;
+    next();
+  }
 };
 
