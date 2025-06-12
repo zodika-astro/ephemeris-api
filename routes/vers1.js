bbb389bdd0dbebe4bc75763e0c813084929f2fa8
Isadora Rocha <info@zodika.com.br>
2025-06-12 12:50:01 +0300

    feat: add POST /v1/ephemeris route

--- routes/vers1.js
+++ routes/vers1.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const EphemerisCntlr = require('../ephemeris/ephemerisCntlr');
-const Validator = require('../ephemeris/validateInput');
+const EphemerisCntlr = require('../controllers/ephemeris');
 
-router.get('/ephemeris', Validator.validate, EphemerisCntlr.get);
-
-router.get('/planetNames', EphemerisCntlr.getPlanetNames);
+router.post('/ephemeris', async function (req, res, next) {
+  try {
+    const result = await EphemerisCntlr.compute(req.body);
+    res.locals.status = 200;
+    res.locals.message = 'Ephemerides returned';
+    res.locals.results = result;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
+
