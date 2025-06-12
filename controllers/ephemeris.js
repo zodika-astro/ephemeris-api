1f9935541110f26054e2f0183fc0bf3210654f4c
Isadora Rocha <info@zodika.com.br>
2025-06-12 15:21:04 +0300

    fix: ensure all input values are numbers

--- controllers/ephemeris.js
+++ controllers/ephemeris.js
@@ -5,8 +5,14 @@ const swisseph = require('swisseph');
 module.exports = {
   compute: function (query) {
     return new Promise((resolve, reject) => {
-      const { year, month, date, hours, minutes, seconds, latitude, longitude } = query;
-
+const year = Number(query.year);
+      const month = Number(query.month);
+      const date = Number(query.date);
+      const hours = Number(query.hours);
+      const minutes = Number(query.minutes);
+      const seconds = Number(query.seconds);
+      const latitude = Number(query.latitude);
+      const longitude = Number(query.longitude);
       const jd = swisseph.swe_julday(year, month, date, hours + minutes / 60 + seconds / 3600, swisseph.SE_GREG_CAL);
 
       swisseph.swe_calc_ut(jd, swisseph.SE_SUN, 0, (err, planet) => {
