2dacb0b175dc157c3ba1af6ba21c3e4d415fece1
Isadora Rocha <info@zodika.com.br>
2025-06-12 14:04:00 +0300

    fix: register controller correctly as middleware

--- routes/vers1.js
+++ routes/vers1.js
@@ -2,17 +2,7 @@ const express = require('express');
 const router = express.Router();
 const EphemerisCntlr = require('../controllers/ephemeris');
 
-router.post('/ephemeris', async function (req, res, next) {
-  try {
-    const result = await EphemerisCntlr.compute(req.body);
-    res.locals.status = 200;
-    res.locals.message = 'Ephemerides returned';
-    res.locals.results = result;
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
+router.post('/ephemeris', EphemerisCntlr.compute);
 
 module.exports = router;
 
