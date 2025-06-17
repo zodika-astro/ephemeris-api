55c1e2b53cffe85bc629344ecd8e29a686d5f3a8
zodika-astro <info@zodika.com.br>
2025-06-17 17:14:37 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
-// Logs when the ephemeris route file is entered.
-console.log('Entered ephemeris route');
+'use strict';
+// Log when this API route file is entered.
+console.log('Entered API route file (routes/api.js)');
 
 const express = require('express');
 const router = express.Router();
@@ -8,25 +9,22 @@ const compression = require('compression'); // Import compression middleware
 const NodeCache = require('node-cache'); // Import node-cache for in-memory caching
 
 // Initialize cache with a TTL (Time To Live) of 600 seconds (10 minutes)
-// for cached items. This means cached responses will be valid for 10 minutes.
 const apiCache = new NodeCache({ stdTTL: 600 });
 
 // Enable Gzip compression for all responses served by this router.
-// This reduces the size of the response body, speeding up data transfer.
 router.use(compression());
 
 // Route to get general API information.
 router.get('/info', (req, res, next) => {
-const cacheKey = 'apiInfo'; // Unique key for caching this response
+  const cacheKey = 'apiInfo'; // Unique key for caching this response
 
-// Try to retrieve data from cache first
-const cachedResponse = apiCache.get(cacheKey);
-if (cachedResponse) {
-    // If found in cache, return immediately
+  // Try to retrieve data from cache first
+  const cachedResponse = apiCache.get(cacheKey);
+  if (cachedResponse) {
     res.locals.status = 200;
     res.locals.message = 'Ephemeris API info successfully retrieved from cache.';
     res.locals.results = cachedResponse;
-    return next(); // Use return next() to stop further execution in this route handler
+    return next();
   }
 
   // If not in cache, fetch data, store it, and then send
@@ -46,7 +44,6 @@ router.get('/health', (req, res, next) => {
   // Try to retrieve data from cache first
   const cachedResponse = apiCache.get(cacheKey);
   if (cachedResponse) {
-    // If found in cache, return immediately
     res.locals.status = 200;
     res.locals.message = 'Ephemeris API health status successfully retrieved from cache.';
     res.locals.results = cachedResponse;
@@ -63,7 +60,7 @@ router.get('/health', (req, res, next) => {
   next();
 });
 
-// Mounts version 1 routes.
+// Mounts version 1 routes under /api/v1.
 router.use('/v1', require('./vers1.js'));
 
 module.exports = router;
