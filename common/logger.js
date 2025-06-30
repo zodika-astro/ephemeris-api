36d69f2e411abb8e9e3c1ad77af818cfc5a98a06
Isadora Rocha <info@zodika.com.br>
2025-06-30 19:01:33 +0300

    Create logger.js

---
+++ common/logger.js
@@ -0,0 +1,14 @@
+const pino = require('pino');
+
+const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname'
+    }
+  }
+});
+
+module.exports = logger;
