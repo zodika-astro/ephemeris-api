570db0b96c71e8b7b87f38c9fcb0ab9db6128bfb
Isadora Rocha <info@zodika.com.br>
2025-07-29 23:29:41 +0200

    Update utils.js

--- common/utils.js
+++ common/utils.js
@@ -1,39 +1,30 @@
 const utils = (function () {
-
     return {
         addDays,
         evaluateError
     };
-
     function addDays(date, days) {
         var tmpDt = new Date(date);
         tmpDt.setDate(tmpDt.getDate() + days);
-    
         return tmpDt;
     }
 
     function evaluateError(err) {
         let message = '';
         let status = undefined;
-
         if (err instanceof Error) {
             message = err.message;
         }
-
         if (err.error) {
             message = err.error.message || err.error || 'Specific message unavailable.';
             status = err.error.status;
         }
-
         if (typeof err == 'string') {
             message = err;
         }
 
         status = status || 400;
-
         return { status, message }
     }
-
 }());
-
 module.exports = utils;
