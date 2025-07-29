62c15fcdb979c35ae144ac7f4170840f07786fec
Isadora Rocha <info@zodika.com.br>
2025-07-29 18:10:11 +0200

    Update generateTableImage.js

--- controllers/generateTableImage.js
+++ controllers/generateTableImage.js
@@ -21,7 +21,7 @@ if (fs.existsSync(interFontPathRegular)) {
 }
 
 // Color and layout constants
-const WIDTH = 1300;
+const WIDTH = 1500;
 const COLORS = {
     BACKGROUND: '#FFFBF4',
     TEXT: '#29281E',
