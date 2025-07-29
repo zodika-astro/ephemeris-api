4908de6d153189932bba38c386b9331c5ac31d61
Isadora Rocha <info@zodika.com.br>
2025-07-29 23:21:51 +0200

    Update info.js

--- common/info.js
+++ common/info.js
@@ -1,29 +1,18 @@
 const fs = require('fs');
-
 const pkg = JSON.parse(fs.readFileSync('package.json'));
-
-
 const info = (function () {
-
     return {
         info,
         health
     };
-
-
     function info() {
-
         return {
             author: pkg.author.name, api: pkg.name, version: pkg.version, status: 'OK',
             'endpoints': ['/api/v1/ephemeris', '/api/v1/planetNames']
         }
     }
-
     function health() {
-
         return 'OK'
     }
-
 }());
-
 module.exports = info;
